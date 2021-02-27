ccfd5cc287c3d81be1b4450bcf1f54ef0e574885
Yuyeol <uy23@naver.com>
2021-02-27 18:15:52 +0900

    라우터변경

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Detail from "screens/Detail";
 import Home from "screens/Home";
 import Search from "screens/Search";
